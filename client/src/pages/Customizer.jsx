07263ec4bfd80964346fd098c2eca5642a78a753
Hitesh Mulwani <hiteshmulwanieng@gmail.com>
2025-01-21 23:29:49 +0530

    add render.com path my replacing localhost

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -48,7 +48,7 @@ const Customizer = () => {
         try {
             setGeneratingImg(true);
     
-            const response = await fetch('http://localhost:8080/api/v1/dalle', {
+            const response = await fetch('https://threed-tshirt-studio-s2uy.onrender.com/api/v1/dalle', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
