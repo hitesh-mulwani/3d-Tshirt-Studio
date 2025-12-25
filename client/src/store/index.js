6fd36e9ad669c7e32e3db22779be461ed6b20ef8
Hitesh Mulwani <hiteshmulwanieng@gmail.com>
2025-12-25 13:10:30 +0530

    update initial default tshirt design

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -6,6 +6,6 @@ const state= proxy({
     isLogoTexture: true,
     isFullTexture: false,
     logoDecal: './apple.png',
-    fullDecal: './t3.png',
+    fullDecal: './w2.png',
 });
 export default state;
